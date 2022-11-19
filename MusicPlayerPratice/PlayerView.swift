ecfa191bf5a9f3f00e16b3657898b8333627b59e
jakkujakku <jakkujakku98@gmail.com>
2022-11-19 13:05:45 +0900

    Add PlayerView code

--- MusicPlayerPratice/PlayerView.swift
+++ MusicPlayerPratice/PlayerView.swift
@@ -5,4 +5,83 @@
 //  Created by (^ㅗ^)7 iMac on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            // 1
+            VStack(spacing:24) {
+                Divider()
+                // 2
+                Image(systemName: "a.square")
+                    .resizable() // 3
+                    .frame(width: geometry.size.width - 24, height: geometry.size.width - 24) // 4
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                
+                VStack(spacing:8) {
+                    Text("Song Title")
+                        .font(Font.system(.title).bold())
+                    Text("Artist Name")
+                        .font(.system(.headline))
+                }
+                
+                HStack(spacing: 40) {
+                    Button(action: {
+                        // 1
+                        print("Rewind")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName: "backward.fill")
+                                .foregroundColor(.white)
+                                .font(.system(.title))
+                        }
+                    }
+                    
+                    Button(action: {
+                        // 1
+                        print("Pause")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName:
+                                    "pause.fill")
+                            .foregroundColor(.white)
+                            .font(.system(.title))
+                        }
+                    }
+                    
+                    Button(action: {
+                        // 1
+                        print("Skip")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName: "forward.fill")
+                                .foregroundColor(.white)
+                                .font(.system(.title))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
